62ad444766a0e8718683aa6af008c31b02d63628
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-06-23 19:25:53 -0300

    feat: configure environment variables and update database connection

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
@@ -10,5 +11,5 @@ app.get('/hello', async () => {
 })
 
 app
-  .listen({ port: 3333 })
+  .listen({ port: env.PORT })
   .then(() => console.log('Server is running on http://localhost:3333/hello'))

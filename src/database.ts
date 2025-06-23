88e119c126d1296aadf7184a90f5ea59bb952d6a
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-06-23 19:12:54 -0300

    feat: add .env.example file and configure database connection to use DATABASE_URL

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,16 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
 
 export const config: Knex.Config = {
   client: 'sqlite3',
   connection: {
-    filename: './database/app.db',
+    filename: process.env.DATABASE_URL || './database/app.db',
   },
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
     directory: './database/migrations',
-  }
+  },
 }
 
 export const knex = setupKnex(config)

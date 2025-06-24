f872960e64ae7b73ca3ba36f41ef70231df038f3
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-06-24 18:19:24 -0300

    style: standardize code formatting in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
-import fastifyCookie from "@fastify/cookie";
-import fastify from "fastify";
-import { env } from "./env";
-import { transationsRoutes } from "./routes/transactions";
+import fastifyCookie from '@fastify/cookie'
+import fastify from 'fastify'
+import { env } from './env'
+import { transationsRoutes } from './routes/transactions'
 
-const app = fastify();
-app.register(fastifyCookie);
+const app = fastify()
+app.register(fastifyCookie)
 app.register(transationsRoutes, {
-	prefix: "transactions",
-});
+  prefix: 'transactions',
+})
 
 app
-	.listen({ port: env.PORT })
-	.then(() => console.log("Server is running on http://localhost:3333/hello"));
+  .listen({ port: env.PORT })
+  .then(() => console.log('Server is running on http://localhost:3333/hello'))

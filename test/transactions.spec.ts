10dd47cd175835fa6d243fd1593f3b4fca6bd1ae
Emanuel Alderete <luisemanuel.alderete@gmail.com>
2025-06-25 11:12:30 -0300

    feat: add test for retrieving a specific transaction in transactions routes

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,6 @@
+import { execSync } from 'node:child_process'
 import request from 'supertest'
-import { afterAll, beforeAll, describe, expect, it } from 'vitest'
-import { string } from 'zod'
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest'
 import { app } from '../src/app'
 
 describe('Transactions routes', () => {
@@ -10,6 +10,11 @@ describe('Transactions routes', () => {
 
 	afterAll(async () => {
 		await app.close()
+	})
+
+	beforeEach(() => {
+		execSync('npm run knex migrate:rollback --all')
+		execSync('npm run knex migrate:latest')
 	})
 
 	it('should be able to create a new transaction', async () => {
@@ -45,5 +50,36 @@ describe('Transactions routes', () => {
 				amount: 5000,
 			}),
 		])
+	})
+
+	it('should be able to get a specific transaction', async () => {
+		const createTransactionResponse = await request(app.server)
+			.post('/transactions')
+			.send({
+				title: 'New transaction',
+				amount: 5000,
+				type: 'credit',
+			})
+
+		const cookies: string = createTransactionResponse.get('Set-Cookie')
+
+		const transactionListResponse = await request(app.server)
+			.get('/transactions')
+			.set('Cookie', cookies)
+			.expect(200)
+
+		const transactionId = transactionListResponse.body.transactions[0].id
+
+		const getTransactionReponse = await request(app.server)
+			.get(`/transactions/${transactionId}`)
+			.set('Cookie', cookies)
+			.expect(200)
+
+		expect(getTransactionReponse.body.transaction).toEqual(
+			expect.objectContaining({
+				title: 'New transaction',
+				amount: 5000,
+			}),
+		)
 	})
 })
